e14f94bf6b0b725fb3e71c581e25c5174d5e3383
Eric Chen <ericchen@Erics-MacBook-Pro.local>
2023-03-24 14:47:42 -0400

    centered intro

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -56,7 +56,8 @@ export default function Home() {
                 </li>
               </ul>
             </nav>
-            <div className='lg:flex'>
+
+            <div className='lg:flex items-center justify-center'>
               <div className='text-center p-10'>
                 <h2 className='text-5xl py-2 text-blue-400 font-medium md:text-6xl'>Eric Chen</h2>
                 <h3 className='text-2xl py-2 md:text-3xl'>Software Developer</h3>
