f287ab3030e6475af051f45e871cdf5507a5e746
Eric Chen <ericchen@Erics-MacBook-Pro.local>
2023-03-25 15:52:44 -0400

    added more padding to top

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -58,7 +58,7 @@ export default function Home() {
             </nav>
 
             <div className='lg:flex items-center justify-center'>
-              <div className='text-center p-10'>
+              <div className='text-center p-10 lg:p-32'>
                 <h2 className='text-5xl py-2 text-blue-400 font-medium md:text-6xl'>Eric Chen</h2>
                 <h3 className='text-2xl py-2 md:text-3xl'>Software Developer</h3>
                 <p className='text-md mx-auto py-5 leading-8 dark:text-gray-300 text-gray-800 md:text-xl max-w-lg'>
